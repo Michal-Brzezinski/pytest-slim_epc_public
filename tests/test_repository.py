d839255809a49070fca58cb8b459eb06f4fc682f
Buczy8 <pawelbuczek48@gmail.com>
2026-05-21 16:21:59 +0200

    unit tests for attach and detach functionality

--- tests/test_repository.py
+++
@@ -1,7 +0,0 @@
-# tests/test_repository.py
-import pytest
-
-def test_cannot_delete_default_bearer(test_repo):
-    test_repo.attach_ue(1)
-    with pytest.raises(ValueError):
-        test_repo.delete_bearer(1, 9)
